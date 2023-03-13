1a2eb63d76586fe7056851c1755e15250c673ee5
Gdel-J <jessygdelvecchio@gmail.com>
2023-03-13 17:22:43 -0400

    Prompts  for user input (and conditions  for theses prompts)  part updated  in the JS file

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ var upperCaseArray = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"
 var lowerCaseArray = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberArray = ["0","1","2","3","4","5","6","7","8"," 9,"];
 
-
 var specialCharacterArray = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
 
 var finalArray = [];
@@ -36,6 +35,23 @@ else {
   alert("Your password will be " + numberCharacter + " characters long.");
   
 }
+var numbers = confirm(" Do you want numbers in your password? ");
+
+  if (numbers) {
+    alert("Your password will have numbers.");
+  }
+  else {
+    alert("Your password will NOT have numbers.");
+  }
+
+var upperCases = confirm("Do you want uppercase characters?");
+
+  if (upperCases) {
+    alert("Your password will have uppercase characters.");
+  }
+  else {
+    alert("Your password will NOT have uppercase characters.");
+  }
 
 
 var lowerCases = confirm("Do you want lowercase characters?");
@@ -58,11 +74,11 @@ var specialCharacters = confirm("Do you want special characters?");
 
   if (lowerCases === false && upperCases === false && numbers === false && specialCharacters === false) {
 
-    return "Please select one charcter type at least !";
+    return "Please select one character type at least !";
   };
 
 
-  //  characters + concat method to combine two arrays and returns a new one 
+  //  characters + concat method to combine two arrays and return a new one 
 
   if (numbers){
      
@@ -83,7 +99,7 @@ var specialCharacters = confirm("Do you want special characters?");
   if (specialCharacters){
     finalArray = finalArray.concat(specialCharacterArray);
   }
-  
+
  // loop  to pick random cards out of new array for length of password
 
  for (var i = 0; i < numberCharacter; i++) {
@@ -95,12 +111,6 @@ var specialCharacters = confirm("Do you want special characters?");
 
   // The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string. If the array has only one item, then that item will be returned without using the separator.
 }
-
-
-
-
-
- 
 
 
 // Write password to the #password input
